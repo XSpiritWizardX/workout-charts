ae3e434f08cb96c14ecbe34dd487e883abf6eb81
Dream Engine <dustinboveemusic@gmail.com>
2026-01-19 05:52:40 -0500

    Update src/features/feature_briefs.js

--- src/features/feature_briefs.js
+++ src/features/feature_briefs.js
@@ -1,15 +1,1 @@
-export const moduleName = "feature_briefs";
-
-export const featureFlags = {
-  onboardingRevamp: true,
-  pricingExperiment: false,
-  retentionAlerts: true,
-  growthPlaybooks: false,
-};
-
-export const isFeatureEnabled = key => Boolean(featureFlags[key]);
-
-export const listEnabledFeatures = () =>
-  Object.keys(featureFlags).filter(key => featureFlags[key]);
-
-export const featureSummary = "Feature Briefs";
+Create a new file `FeatureBriefs.js` and add the necessary code to render the feature briefs
