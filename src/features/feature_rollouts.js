87ca7c89a033f921ea6150db3a4a268e15099887
Dream Engine <dream-engine@local>
2026-01-19 03:48:16 -0500

    Add src/features/feature_rollouts.js

---
+++ src/features/feature_rollouts.js
@@ -0,0 +1,15 @@
+export const moduleName = "feature_rollouts";
+
+export const featureFlags = {
+  onboardingRevamp: true,
+  pricingExperiment: false,
+  retentionAlerts: true,
+  growthPlaybooks: false,
+};
+
+export const isFeatureEnabled = key => Boolean(featureFlags[key]);
+
+export const listEnabledFeatures = () =>
+  Object.keys(featureFlags).filter(key => featureFlags[key]);
+
+export const featureSummary = "Feature Rollouts";
